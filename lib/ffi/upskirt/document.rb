183912e98148e814a8ef3e6d4c4a405bd6bb52ea
Postmodern <postmodern.mod3@gmail.com>
2011-05-11 01:08:34 -0700

    Added a skeleton Document class.

---
+++ lib/ffi/upskirt/document.rb
@@ -0,0 +1,14 @@
+module FFI
+  module Upskirt
+    class Document
+
+      def initialize(source='',options={})
+        @source = source
+      end
+
+      def to_html
+      end
+
+    end
+  end
+end
