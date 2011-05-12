794d843b4589c79cf41516c6d4392e74a63d5835
Postmodern <postmodern.mod3@gmail.com>
2011-05-11 19:55:59 -0700

    Added Buffer#to_smartypants.

--- lib/ffi/upskirt/buffer.rb
+++ lib/ffi/upskirt/buffer.rb
@@ -1,4 +1,4 @@
-require 'ffi'
+require 'ffi/upskirt/upskirt'
 
 module FFI
   module Upskirt
@@ -9,6 +9,16 @@ module FFI
              :asize, :size_t,
              :unit, :size_t,
              :ref, :int
+
+      def to_smartypants
+        smart_buffer = Upskirt.bufnew(128)
+
+        Upskirt.upshtml_smartypants(smart_buffer,self)
+        result = smart_buffer.data.get_string(0,smart_buffer.size)
+
+        smart_buffer.free
+        return result
+      end
 
     end
   end
